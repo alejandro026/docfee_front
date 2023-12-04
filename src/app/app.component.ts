db05e2e400e2f5d65764d6143e3c5c53dcbd8d1f
ARGL <92689016+alejandro026@users.noreply.github.com>
2023-12-03 22:29:02 -0600

    Agregando notificaciones

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component, HostListener } from '@angular/core';
 import { InactivityService } from './services/inactivity.service';
+import { SwPush } from '@angular/service-worker';
+import { NotificationService } from './services/notification.service';
 
 @Component({
   selector: 'app-root',
@@ -9,7 +11,32 @@ import { InactivityService } from './services/inactivity.service';
 export class AppComponent {
   title = 'DOCFEE';
 
-  constructor(private inactivityService: InactivityService) {}
+  //Llave publica para las notificaciones push
+  public readonly VALID_PUBLIC_KEY="BNbKdoGiuuGcZzadTU6KPxfKXgqvaIQs7bKkchFA9T9UM1oJEukHJeKqkUNDHZKRDqdeC5mEUDVObu9_A-YNh6U";
+
+  constructor(
+    private inactivityService: InactivityService,
+    private swPush: SwPush,
+    private notificacionService: NotificationService
+    ) {
+      this.suscribeToNotifications();
+    }
+
+  suscribeToNotifications():any{
+    this.swPush.requestSubscription({
+      serverPublicKey: this.VALID_PUBLIC_KEY
+    }).then(sub =>{
+      const token = JSON.parse(JSON.stringify(sub));
+      console.log("El token");
+      console.log(token);
+      this.notificacionService.guardarToken(token).subscribe((data:any)=>{
+        console.log("Se guardo el token");
+      });
+    }).catch(error=>{
+      //En caso de que no acepte las notificaciones
+      console.log("UPS :( "+error);
+    })
+  }
 
   @HostListener('document:click', ['$event'])
   @HostListener('document:mousemove', ['$event'])
--- src/app/services/notification.service.ts
+++ src/app/services/notification.service.ts
@@ -1,12 +1,16 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import {MatSnackBar, MatSnackBarConfig} from '@angular/material/snack-bar';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NotificationService {
 
-  constructor(public snackBar: MatSnackBar) { }
+  url="https://docfeeback.hop.sh";
+
+  constructor(public snackBar: MatSnackBar,  private http: HttpClient) { }
 
   config: MatSnackBarConfig = {
     duration: 3000,
@@ -22,4 +26,10 @@ export class NotificationService {
     this.config['panelClass'] = ['notification', 'warn'];
     this.snackBar.open(msg, '', this.config)
   }
+
+  guardarToken(token:any):Observable<any>{
+    return this.http.post<any>(this.url+"/notificaciones/guardar",token,
+    {headers: new HttpHeaders().append("Content-Type", "application/json")});
+  }
+
 }
